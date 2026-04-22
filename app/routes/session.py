175d832a2652cda82239a6b8ef04aff49f427bac
Nick Killeen <sirkillz1198@gmail.com>
2026-04-21 20:48:55 -0500

    Created relationship between sessions and players and created new route to get all players within a given session

--- app/routes/session.py
+++ app/routes/session.py
@@ -9,6 +9,7 @@ from app.database.session_factory import get_db
 from app.database.models import ContractSongSession
 
 from app.schemas.session import ReadSession, CreateSession
+from app.schemas.player import ReadPlayer
 from app.routes.helpers import parse_str_to_datetime, get_new_access_token_expiration
 
 session_router = APIRouter(prefix="/api/v1/session", tags=["Session"])
@@ -29,6 +30,19 @@ async def get_session(session_id: int, db: Session = Depends(get_db)):
 
     return result
 
+@session_router.get("/{session_id}/players", response_model=list[ReadPlayer])
+async def get_session_players(session_id, db: Session = Depends(get_db)):
+
+    logger.info(f"Received Request at GET: /api/v1/session{session_id}/players")
+
+    stmt = select(ContractSongSession).where(ContractSongSession.id == session_id)
+    contract_song_session = db.execute(stmt).scalar_one_or_none()
+
+    if not contract_song_session:
+        raise HTTPException(status_code=404, detail=f"Session with id: {session_id} not found")
+
+    players = contract_song_session.players
+    return players
 
 
 @session_router.post("", response_model=ReadSession)
