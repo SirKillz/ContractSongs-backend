2d5ace3a243227b9a92097c0373319dfc1506f25
Nick Killeen <sirkillz1198@gmail.com>
2026-04-26 08:41:03 -0500

    Polling plus combined audio working as of now

--- app/services/aws_polly.py
+++ app/services/aws_polly.py
@@ -1,8 +1,22 @@
 import os
+from io import BytesIO
+from pathlib import Path
 from dataclasses import dataclass
 
 import boto3
 from botocore.response import StreamingBody
+
+from pydub import AudioSegment
+
+INPUT_AUDIO_DIR = Path("contract-song-audio-in")
+OUTPUT_AUDIO_DIR = Path("contract-song-audio-out")
+ALERT_SOUND_PATH = INPUT_AUDIO_DIR / "smash_challenger_alert.mp3"
+
+# Decode once when the module loads
+ALERT_SOUND_AUDIO = AudioSegment.from_file(
+    ALERT_SOUND_PATH,
+    format="mp3"
+)
 
 @dataclass
 class PollyResponse:
@@ -35,11 +49,24 @@ def generate_polly_audio_file(text: str) -> PollyResponse:
     )
     return polly_response
 
-def write_polly_audio_file(polly_response: PollyResponse, output_filename: str):
-    """
-    output_filename should be a combo of {SpotifySong.id}-{players}
-    """
-    audio_bytes = polly_response.AudioStream.read()
+def write_combined_audio_file(polly_response: PollyResponse, output_filename: str) -> str:
+    if not output_filename.endswith(".mp3"):
+        output_filename += ".mp3"
 
-    with open(f"contract-song-audio/{output_filename}", "wb") as f:
-        f.write(audio_bytes)
+    output_path = OUTPUT_AUDIO_DIR / output_filename
+
+    polly_bytes = polly_response.AudioStream.read()
+
+    polly_audio = AudioSegment.from_file(
+        BytesIO(polly_bytes),
+        format="mp3"
+    )
+
+    combined_audio = ALERT_SOUND_AUDIO + polly_audio
+
+    combined_audio.export(
+        output_path,
+        format="mp3"
+    )
+
+    return str(output_path)
